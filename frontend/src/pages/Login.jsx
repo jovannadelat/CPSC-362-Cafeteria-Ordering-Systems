814359229fafd23f5fe0ebeddbfcf1035643ed76
Jovanna <jovannadelatrinidad@gmail.com>
2025-03-28 16:39:55 -0700

    added frontend components, files, and code to Login/SignUp page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,25 +1,26 @@
 import { Link } from 'react-router-dom';
 import { useId } from 'react';
-
+import LoginSignup from '../components/LoginSignup/LoginSignup';
 function Login() {
     return (
-        <form>
-            <h2>Please Enter Company Username and Password</h2>
-            <label>
-                Your Username:
-                <input name="userName" />
-            </label>
-            <br />
-            <label>
-                Your Password: 
-                <input name="userPassword" />
-            </label>
-            <br />
-            <button>
-                <Link to="/home">Log In</Link>
-            </button>
+        <LoginSignup/>
+        // <form>
+        //     <h2>Please Enter Company Username and Password</h2>
+        //     <label>
+        //         Your Username:
+        //         <input name="userName" />
+        //     </label>
+        //     <br />
+        //     <label>
+        //         Your Password: 
+        //         <input name="userPassword" />
+        //     </label>
+        //     <br />
+        //     <button>
+        //         <Link to="/home">Log In</Link>
+        //     </button>
 
-        </form>
+        // </form>
 
     );
 
