0250b8c05bceb6e5d8e2d4a39d1bc255fa252887
ManicMondays35 <149739285+ManicMondays35@users.noreply.github.com>
2025-05-05 01:06:41 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,23 @@ import Menu from './pages/Menu-Page/Menu';
 import OrderHistory from './pages/OrderHistory';
 import Checkout from './pages/Checkout-Page/Checkout';
 import { CartProvider } from './pages/Checkout-Page/CartContext';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/order-history" element={<OrderHistory />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </CartProvider>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/order-history" element={<OrderHistory />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
