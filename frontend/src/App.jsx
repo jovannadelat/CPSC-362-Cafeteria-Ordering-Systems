a54d0f750a8eaa3cb0d6cbf3db5210efe01b854c
ManicMondays35 <149739285+ManicMondays35@users.noreply.github.com>
2025-05-05 01:03:22 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,27 +4,22 @@ import Home from './pages/Home';
 import Menu from './pages/Menu-Page/Menu';
 import OrderHistory from './pages/OrderHistory';
 import Checkout from './pages/Checkout-Page/Checkout';
-import { CartProvider } from './pages/Checkout-Page/CartContext'; // Import CartProvider
-import "./App.css";
+import { CartProvider } from './pages/Checkout-Page/CartContext';
+import './App.css';
 
 function App() {
   return (
-    <CartProvider> {/* Wrap your app in CartProvider */}
-      <Router>
-        <div>
-          <Routes>
-            { /* Home page (where buttons will appear) */}
-            <Route path="/" element={<Login />} />
-
-            {/* Other Pages */}
-            <Route path="/home" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/order-history" element={<OrderHistory />} />
-            <Route path="/checkout" element={<Checkout />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+    <Router>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </CartProvider>
+    </Router>
   );
 }
 
