b797ada782e77b9537c10437d057b47751c4c74a
ManicMondays35 <149739285+ManicMondays35@users.noreply.github.com>
2025-05-05 23:33:11 -0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
+
     const containerStyle = {
         backgroundImage: 'url("https://images.unsplash.com/photo-1730780883153-b3c046b001c1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
         backgroundSize: 'cover',
@@ -42,6 +44,11 @@ function Home() {
         cursor: 'pointer',
         textDecoration: 'none',
         transition: 'background-color 0.3s',
+        textAlign: 'center',
+    };
+
+    const handleLogout = () => {
+        window.location.href = 'http://localhost:5173/';
     };
 
     return (
@@ -52,6 +59,8 @@ function Home() {
                 <div style={buttonContainerStyle}>
                     <Link to="/menu" style={buttonStyle}>View Menu</Link>
                     <Link to="/order-history" style={buttonStyle}>Order History</Link>
+                    <button style={buttonStyle} onClick={() => {}}>Modify Menu</button>
+                    <button style={buttonStyle} onClick={handleLogout}>Logout</button>
                 </div>
             </div>
         </div>
