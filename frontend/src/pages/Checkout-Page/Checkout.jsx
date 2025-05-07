09b8ab9058db0ade9459c74677bf071613736a8c
ManicMondays35 <149739285+ManicMondays35@users.noreply.github.com>
2025-05-07 00:37:50 -0700

    Update Checkout.jsx

--- frontend/src/pages/Checkout-Page/Checkout.jsx
+++ frontend/src/pages/Checkout-Page/Checkout.jsx
@@ -4,10 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import { useCart } from "../Checkout-Page/CartContext";
 
 const CheckoutContainer = styled.div`
-  backgroundImage: 'url("https://images.unsplash.com/photo-1730780883153-b3c046b001c1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
   display: flex;
   justify-content: space-between;
   padding: 20px;
+  background-image: url("https://images.unsplash.com/photo-1730780883153-b3c046b001c1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
 `;
 
 const CartItemsBox = styled.div`
