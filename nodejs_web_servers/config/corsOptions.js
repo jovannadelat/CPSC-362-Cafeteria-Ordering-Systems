c975f26416e444d27f51edcb6aae8984f22410be
Jovanna <jovannadelatrinidad@gmail.com>
2025-05-08 20:53:31 -0700

    fixed log in bug

--- nodejs_web_servers/config/corsOptions.js
+++ nodejs_web_servers/config/corsOptions.js
@@ -2,13 +2,16 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log('🌍 Incoming Origin:', origin); // Add this
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+            callback(null, true);
         } else {
+            console.error('⛔ Blocked Origin:', origin);
             callback(new Error('Not allowed by CORS'));
         }
     },
+    credentials: true,
     optionsSuccessStatus: 200
-}
+};
 
 module.exports = corsOptions;
--- nodejs_web_servers/middleware/credentials.js
+++ nodejs_web_servers/middleware/credentials.js
@@ -3,9 +3,10 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', origin); // ✅ Must include this
+        res.header('Access-Control-Allow-Credentials', 'true');
     }
     next();
-}
+};
 
-module.exports = credentials
+module.exports = credentials;
